1cd8095ee201162d8a0a0aea7f5fdf4fd1987fb0
the-legend-sinmayan <sinmayankokul693@gmail.com>
2026-03-23 18:33:56 +0200

    first commit

---
+++ lesson1/print.py
@@ -0,0 +1,4 @@
+print("Hello, world")
+print("My name is Sinmayan")
+print("I love playing soccer")
+print("the result of 3+5 is:",3 + 5)
---
+++ lesson1/world_of_programming.py
@@ -0,0 +1,8 @@
+print("welcome to the world of programming!")
+
+print(4)
+
+print("Hello Python \n")
+print("keywords in python are.....")
+import keyword
+print (keyword.kwlist)
