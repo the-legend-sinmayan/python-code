c7b48361ed051b9589e54b171a244db280f00b10
the-legend-sinmayan <sinmayankokul693@gmail.com>
2026-03-31 13:43:13 +0200

    commit files

---
+++ homework.py/exam.py
@@ -0,0 +1,27 @@
+name= "sinmayan kokul"
+age=13
+is_student= True
+exam_eligibility= "yes"
+
+print("name:", name)
+print("age:", age)
+print("are you a student: ", is_student)
+print("exam eligibility:", exam_eligibility)
+
+print("name:", name)
+print("Data type of name is", type(name))
+print("age:", age)
+print("Data type of age is", type(age))
+print("are you a student:", is_student)
+print("Data type of student is", type(is_student))
+print("exam eligibility:", exam_eligibility)
+print("Data type of exam eligibility is", type(exam_eligibility))
+
+print("\n After type casting..........")
+age = str(age)
+print(age)
+print("dataTypeof age is",type(age))
+exam_eligibility = int(exam_eligibility)
+print(exam_eligibility)
+
+print("Data type of exam eligibility is", type(exam_eligibility))
