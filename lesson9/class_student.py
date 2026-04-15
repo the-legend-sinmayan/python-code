e70933c26f712c9f2bea4e9a2f373345fbcf3df6
the-legend-sinmayan <sinmayankokul693@gmail.com>
2026-04-15 18:26:53 +0200

    commit files

---
+++ lesson9/class_student.py
@@ -0,0 +1,25 @@
+class student:
+    grade = 10
+    print("Hi i am a student of grade", grade)
+
+ob = student()
+
+class student:
+    country = "Sri Lanka"
+    print("Hi i am from ", country)
+
+ob = student()
+
+class student:
+    grade = 10
+    name = "sinmayan"
+    def introduction(self):
+        print("Hi i am a student")
+
+    def details(self):
+        print("My name is ", self.name)
+        print("I am in grade ", self.grade)
+
+ob = student()
+ob.introduction()
+ob.details()
