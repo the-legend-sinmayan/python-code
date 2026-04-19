1b3b7857610ed047aaed4d337610c88924c137f6
the-legend-sinmayan <sinmayankokul693@gmail.com>
2026-04-19 19:04:35 +0200

    commit files

---
+++ homework.py/tuple.py
@@ -0,0 +1,21 @@
+
+
+class student:
+    country = "Sri Lanka"
+    print("Hi i am from ", country)
+
+ob = student()
+
+class student:
+    grade = 10
+    name = "sinmayan"
+    def introduction(self):
+        print("Hi i am a student")
+
+    def details(self):
+        print("My name is ", self.name)
+        print("I am in grade ", self.grade)
+
+ob = student()
+ob.introduction()
+ob.details()
