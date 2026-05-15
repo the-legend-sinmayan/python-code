1671bd41b154114754c0dbc4908901d173c9b9d8
the-legend-sinmayan <sinmayankokul693@gmail.com>
2026-05-15 14:38:21 +0200

    commit files

---
+++ homework.py/shubangi.py
@@ -0,0 +1,11 @@
+def myfunction(n):
+    for i in range(0,n+1):
+        print("First Loop")
+ 
+    j=1
+    while(j<=n+1):
+        print("Second Loop ",j)
+        j=j*2
+ 
+    for i in range(0,100):
+        print("Third loop")
