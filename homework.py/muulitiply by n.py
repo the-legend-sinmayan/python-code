061abaefd34aa4589740b2ed08cf6e0405ec661a
the-legend-sinmayan <sinmayankokul693@gmail.com>
2026-05-15 14:35:44 +0200

    commit files

---
+++ homework.py/muulitiply by n.py
@@ -0,0 +1,10 @@
+
+def printnumber(n):
+    iteration=0
+    print("The number entered by the user is",n)
+    iteration+=30
+    print("Total iterations doneby the code is",iteration,"\n")
+
+    printnumber(5)
+    printnumber(6)
+    print("\n with any 'n' the time taken by our code wont change")
